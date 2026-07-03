6ac9f2cbda8da413598f0d4d923e90beac17e3d7
maheshpanta333 <pantamahesh99@gmail.com>
2026-07-03 13:44:50 +0545

    added cors just becausae why not

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from database import engine, Base
 from auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # your frontend's exact address
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
