dbe50fa13cdc627c5253c1739ac91372e564487a
maheshpanta333 <pantamahesh99@gmail.com>
2026-07-03 12:01:33 +0545

    some backend files

---
+++ backend/schemas.py
@@ -0,0 +1,20 @@
+#this for data validation
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class SignUpUsers(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    phone_num: Optional[str] = None
+    address: Optional[str] = None
+
+
+class loginrequest(BaseModel):
+    email:EmailStr
+    password:str
+
+
+class TokenRequest(BaseModel):
+    access_token: str
+    token_type: str="bearer"
---
+++ backend/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer, Column, String
+from database import Base
+
+class User(Base):
+    __tablename__="users"
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)   # will store the HASH, never plain text
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone_num = Column(String, nullable=True)
+    address = Column(String, nullable=True)
